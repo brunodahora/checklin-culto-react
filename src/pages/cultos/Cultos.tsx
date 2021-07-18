3baa50d7332b6619468f5c0a62570d548b633eb8
Bruno Vercelino da Hora <bruno.hora@pipefy.com>
2021-07-18 12:27:38 -0300

    Adding empty state and loading to cultos list

--- src/pages/cultos/Cultos.tsx
+++ src/pages/cultos/Cultos.tsx
@@ -1,31 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-import { Box, Card, CardContent, Typography } from "@material-ui/core";
 // import { useQuery } from "react-query";
+import { Box, Typography } from "@material-ui/core";
 import { Cultos as CultosType } from "../../index.d";
+import CultosList from "./CultosList";
 // import { buscaCultos } from "../../queries/api";
-
-const StyledLink = styled(Link)`
-  flex: 1;
-  text-decoration: none;
-  font-weight: bold;
-  color: black;
-`;
-
-const StyledCardContent = styled(CardContent)`
-  display: flex;
-  background-color: white;
-
-  :hover,
-  :focus {
-    background-color: lightgray;
-  }
-`;
-
-const ScrollContainer = styled.div`
-  overflow: auto;
-`;
 
 export default function Cultos(): React.ReactElement {
   // const { isLoading, error, data, isFetching } = useQuery<CultosType, Error>("cultos", buscaCultos);
@@ -35,26 +13,14 @@ export default function Cultos(): React.ReactElement {
   // // eslint-disable-next-line no-console
   // if (error) console.error(`An error has occurred: ${error.message}`);
 
-  // if (!data?.mensagem || !Object.keys(data.mensagem)) return <div>Não existem cultos cadastrados para hoje.</div>;
-
   const cultos: CultosType = { mensagem: { "1": "teste", "2": "teste 2" } };
 
   return (
     <Box display="flex" flexDirection="column" flex={1} p={2} height="calc(100vh - 32px)">
       <Typography variant="h1">Cultos</Typography>
-      <ScrollContainer>
-        {Object.keys(cultos.mensagem).map((key) => (
-          <Box key={key} mb={2}>
-            <Card>
-              <StyledCardContent>
-                <StyledLink to={`/cultos/${key}?name=${cultos.mensagem[key]}`}>
-                  <Typography variant="button">{cultos.mensagem[key]}</Typography>
-                </StyledLink>
-              </StyledCardContent>
-            </Card>
-          </Box>
-        ))}
-      </ScrollContainer>
+      <Box overflow="hidden" flex={1} display="flex" flexDirection="column">
+        <CultosList cultos={cultos} />
+      </Box>
     </Box>
   );
 }
---
+++ src/pages/cultos/CultosList.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import HighlightOffIcon from "@material-ui/icons/HighlightOff";
+import { Box, Card, CardContent, CircularProgress, Typography } from "@material-ui/core";
+import { Cultos as CultosType } from "../../index.d";
+
+const StyledLink = styled(Link)`
+  flex: 1;
+  text-decoration: none;
+  font-weight: bold;
+  color: black;
+`;
+
+const StyledCardContent = styled(CardContent)`
+  display: flex;
+  background-color: white;
+
+  :hover,
+  :focus {
+    background-color: lightgray;
+  }
+`;
+
+type Props = {
+  cultos: CultosType | null;
+  loading?: boolean;
+};
+
+export default function CultosList({ cultos, loading = false }: Props): React.ReactElement {
+  if (loading)
+    return (
+      <Box display="flex" flexDirection="column" flex={1} alignItems="center" justifyContent="center" mt={2}>
+        <CircularProgress color="primary" size={64} />
+      </Box>
+    );
+
+  if (!cultos?.mensagem || !Object.keys(cultos.mensagem))
+    return (
+      <Box display="flex" flexDirection="column" flex={1} alignItems="center" justifyContent="center" mt={2}>
+        <HighlightOffIcon fontSize="large" />
+        <Typography variant="body1">Não existem cultos cadastrados para hoje.</Typography>
+      </Box>
+    );
+
+  return (
+    <>
+      {Object.keys(cultos.mensagem).map((key) => (
+        <Box key={key} mb={2}>
+          <Card>
+            <StyledCardContent>
+              <StyledLink to={`/cultos/${key}?name=${cultos.mensagem[key]}`}>
+                <Typography variant="button">{cultos.mensagem[key]}</Typography>
+              </StyledLink>
+            </StyledCardContent>
+          </Card>
+        </Box>
+      ))}
+    </>
+  );
+}
+CultosList.defaultProps = {
+  loading: false,
+};
